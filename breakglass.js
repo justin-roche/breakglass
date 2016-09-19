50a76e0db195fb7b512fc0ba903a143535617bdd
jonathan.deng <dengjonathan@gmail.com>
2016-09-19 16:25:39 -0400

    allowed sub-directory as command line argument

--- breakglass.js
+++ breakglass.js
@@ -5,71 +5,87 @@
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-function breakGlass(){
+function breakGlass() {
 
   var htmlFileName = '/fastIndex.html';
   //the names of the scripts
-  var scriptNames = ""; 
+  var scriptNames = "";
 
   //the script tags
   var scripts = "";
 
-  var directory = process.cwd(); 
+  var directory = process.cwd();
 
   //the input command [filename | undefined]
   var command = process.argv.slice(2)[0];
+  var commandIsJS = false;
 
   //if a command is provided and it is a .js filename, add it to the scripts
-  if(command && isJsFileString(command)){
+  if (command && isJsFileString(command)) {
     scripts = `<script src="${command}"></script>`;
     scriptNames = command;
+    commandIsJS = true;
   }
 
-  //if no command, add references to all detected .js files in the newly created index html
-  if(!command){
+  //if a command is provided and it is a sub-directory in current working directory run breakglass
+  //on sub-directory provided
+  if (command && isDirectory(command)) {
+    directory = `${directory}/${command}`;
+    console.log(`running breakglass on the sub-directory ${directory}`);
+  }
+
+  //if no command is not js file (it could be a directory), add references to all detected .js files in the newly created index html
+  if (!commandIsJS) {
     //get all files
     var files = fs.readdirSync(directory);
     //filter to only .js
-    jsFiles = files.filter(function(name){
-      return isJsFileString(name);
-    })
-    //create script reference for each file
-    jsFiles.forEach(function(fileName){
+    jsFiles = files.filter(function (name) {
+        return isJsFileString(name);
+      })
+      //create script reference for each file
+    jsFiles.forEach(function (fileName) {
       var script = `<script src="${fileName}"></script>`;
       scriptNames = scriptNames + '  ' + fileName;
       scripts = scripts + '\n' + script;
-      
+
     });
 
-  } 
+  }
 
-  htmlString = getTemplate(scriptNames,scripts);
+  htmlString = getTemplate(scriptNames, scripts);
 
   //test whether a file is a .js file
-  function isJsFileString(name){
-    return name.slice(name.length-3) === '.js';
+  function isJsFileString(name) {
+    return name.slice(name.length - 3) === '.js';
+  }
+
+  // test whether Command line argument is a directory
+  function isDirectory(name) {
+    return fs.readdirSync(directory).filter(function (file) {
+      return fs.statSync(file).isDirectory();
+    });
   }
 
   //create the file in the current directory
-  fs.writeFile(directory+htmlFileName, htmlString, function(err) {
-      if(err) {
-        console.log('an error');
-          return console.log(err);
-      }
-      
-      console.log("The file" + htmlFileName +" was saved!");
-      spawn('open', [directory+htmlFileName]);
-  }); 
+  fs.writeFile(directory + htmlFileName, htmlString, function (err) {
+    if (err) {
+      console.log('an error');
+      return console.log(err);
+    }
+
+    console.log("The file" + htmlFileName + " was saved!");
+    spawn('open', [directory + htmlFileName]);
+  });
 
   //fill the template string with the scripts tag and script names
-  function getTemplate(scriptNames,scripts){
+  function getTemplate(scriptNames, scripts) {
 
     var htmlString = `<!DOCTYPE>
       <html>
-        
+
         <head>
           <meta charset="UTF-8">
-          
+
         </head>
 
       <body >
@@ -80,7 +96,7 @@ function breakGlass(){
         ${scripts}
 
       </body>
-      
+
       </html>`;
 
     return htmlString;
@@ -90,4 +106,3 @@ function breakGlass(){
 
 //makes into a node module
 module.exports = breakGlass();
-
