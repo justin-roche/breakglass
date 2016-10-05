f4e7763a59a9d92bb66e81ed54b8aa164f6d1f8f
Justin <jproche5@gmail.com>
2016-10-04 22:43:45 -0400

    merge 2.0

--- README.md
+++ README.md
@@ -1,34 +1,24 @@
 # breakglass
-an executable script that creates an index.html in the current directory. All .js files in the current directory are loaded as scripts of the newly created .html file. The .html is then opened with default web browser.
+A command line tool for creating mocha specs and html files from directories of existing javascript files. All .js files in the current directory are loaded as scripts of the newly created .html file. The .html is then opened with the default web browser.
 
 # installation and use
 open the directory in terminal and run 'npm install . -g'. The script should now be available everywhere using the command $breakglass.
 
-breakglass can accept two types of command line arguments.
+Breakglass can accept three types of command line arguments:
 
-## 1. If provided an argument of the form filename.js, it will initialize the newly created index.html to load only that script
+#$breakglass [ -r | -rm | --mocha ] 
 
-Example:
+1. If provided no arguments it will create an html file called _specRunner.html that loads all .js files in the current directory
 
-`breakglass filename.js`
+2. If provided the -r flag the created _specRunner file will load all .js files in the current directory and in subdirectories, recursively.
 
-## 2. If provided a sub-directory of the current working directory, breakglass will create a new index.html with all files in that sub directory.
+3. If provided the --mocha flag, it will create, in addition to the _specRunner file, a _spec.js file with prewritten describe and it statements based on the included .js files. The required dependencies are added to the _specRunner file.
 
-Example:
-
-File structure:
-| mainDirectory/
-  | subDirectory/
-    | app.js
-    | test.js
-
-Calling the below command from mainDirectory will load all js scripts in the subDirectory.
-
-`breakglass subDirectory`
-
+4. If provided the -rm argument, it will delete any previously created _spec.js and _specRunner files  
 
 # issues
-There is no -rm command, to remove the new index.html file you will have to remove it manually
+1. .gitignore flags are not set by breakglass. Run breakglass -rm to remove files before commiting. 
+
 
 
 
--- breakglass.js
+++ breakglass.js
@@ -5,102 +5,288 @@
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
+//functions modify this settings variable
+var settings = {
+    htmlFileName: '/_specRunner.html',
+    specFileName: '/_spec.js',
+    creationDirectory: process.cwd(),
+    targetDirectories: [process.cwd()],
+    commandMode: '-fl',       //default command mode is flat (not recursive)
+    createdFiles: [],         //saves filepath of created files
+    commandOptions: [],       //includes (--mocha) to create spec file
+    targetFiles: [],          //file paths of .js files to test
+  };
+
 function breakGlass() {
+  getArgs();
 
-  var htmlFileName = '/fastIndex.html';
-  //the names of the scripts
-  var scriptNames = "";
-
-  //the script tags
-  var scripts = "";
-
-  var directory = process.cwd();
-
-  //the input command [filename | undefined]
-  var command = process.argv.slice(2)[0];
-  var commandIsJS = false;
-
-  //if a command is provided and it is a .js filename, add it to the scripts
-  if (command && isJsFileString(command)) {
-    scripts = `<script src="${command}"></script>`;
-    scriptNames = command;
-    commandIsJS = true;
-  }
-
-  //if a command is provided and it is a sub-directory in current working directory run breakglass
-  //on sub-directory provided
-  else if (command && isDirectory(command)) {
-    directory = `${directory}/${command}`;
-    console.log(`running breakglass on the sub-directory ${directory}`);
-  }
-
-  //if no command is not js file (it could be a directory), add references to all detected .js files in the newly created index html
-  if (!commandIsJS) {
-    //get all files
-    var files = fs.readdirSync(directory);
-    //filter to only .js
-    jsFiles = files.filter(function (name) {
-        return isJsFileString(name);
-      })
-      //create script reference for each file
-    jsFiles.forEach(function (fileName) {
-      var script = `<script src="${fileName}"></script>`;
-      scriptNames = scriptNames + '  ' + fileName;
-      scripts = scripts + '\n' + script;
-
-    });
-
-  }
-
-  htmlString = getTemplate(scriptNames, scripts);
-
-  //test whether a file is a .js file
-  function isJsFileString(name) {
-    return name.slice(name.length - 3) === '.js';
-  }
-
-  // test whether Command line argument is a directory
-  function isDirectory(name) {
-    return fs.readdirSync(directory).filter(function (file) {
-      return fs.statSync(file).isDirectory();
-    });
-  }
-
-  //create the file in the current directory
-  fs.writeFile(directory + htmlFileName, htmlString, function (err) {
-    if (err) {
-      console.log('an error');
-      return console.log(err);
+  if(settings.commandMode === '-rm'){   //deletion
+    removeFiles();
+  } else {                              //creation
+    getDirectories();                   //produce a list of all directories
+    getFiles();                         //get the files in those directories
+  
+    if(settings.commandOptions.indexOf("--mocha") !== -1){
+      makeSpecString();                 //apply the templating for a _spec file
+      writeSpecFile();  
+      settings.targetFiles.push(settings.creationDirectory + settings.specFileName); //!should be async after file creation
+      makeScriptsString();
+      makeMochaHtmlString();                
+    } else {
+      makeScriptsString();                //apply the templating for the scripts in the html
+      makeHtmlString();
     }
 
-    console.log("The file" + htmlFileName + " was saved!");
-    spawn('open', [directory + htmlFileName]);
+    writeIndex();                       //write the specRunner file
+    //flagForIgnore();
+  }
+  
+}
+
+function removeFiles(){
+  var targets = [settings.creationDirectory + settings.htmlFileName, settings.creationDirectory + settings.specFileName];
+
+  targets.forEach(function(path){
+    fs.unlink(path, function(err) {               
+      if (err) {
+        //error occurs if either of the files don't exist
+      } else {
+        console.log(`${nameFromPath(path)} deleted successfully!`);
+      }
+    });
   });
+}
 
-  //fill the template string with the scripts tag and script names
-  function getTemplate(scriptNames, scripts) {
+function getArgs(){
+  var args = process.argv.slice(2);
 
-    var htmlString = `<!DOCTYPE>
-      <html>
+  args.forEach(function(c){
 
-        <head>
-          <meta charset="UTF-8">
+    //if it is a command mode
+    if(c[0]==="-" && c[1] !== "-"){
+      settings.commandMode = c; 
+    }
 
-        </head>
+    //if it is a command option...
+    else if(c[0]==="-" && c[1] === "-"){     
+      if(typeof settings.commandOptions === 'undefined'){
+        settings.commandOptions = [];
+      }
+      settings.commandOptions.push(c);
+    }
 
-      <body >
-        <h1> Scripts Running</h1>
-        <p> ${scriptNames} </p>
+    //if it is a filename
+    else if(isJsFileString(c)){
+      if(typeof settings.targetFiles === 'undefined'){
+        settings.targetFiles = [];
+      }
+      settings.targetFiles.push(c);         
+    }  
+    
+    //if it is a directory name
+    else if (isDirectory(c)) {
+        if(typeof settings.targetDirectories === 'undefined'){
+          settings.targetDirectories = [];
+        }
+        settings.targetDirectories.push(c);
+    }  
+
+    //if none of the above, throw error
+    else {
+        throw new Error('invalid argument: use [dir | file | -mode | --option]');
+      }
+  });
+    
+}
+
+///////////////////FILE NAVIGATION/GETTING FILES
+
+function getDirectories(){
+  //if not recursive mode
+  if(settings.commandMode === '-fl'){
+
+  } 
+  //if recursive mode, recurse
+  if(settings.commandMode === '-r'){
+    recurseDirectories(settings.targetDirectories)
+  }
+}
+
+function recurseDirectories(directories){
+  //breadth-first search
+  var queue = directories;
+
+  while(queue.length > 0){
+    var dir = queue.pop();
+    var containedDirs = getSubDirectories(dir);       //saved as paths
+    queue = containedDirs.concat(queue);
+    settings.targetDirectories.push(dir);       
+  }
+
+}
+
+function getSubDirectories(directory){
+  var containedPaths = fs.readdirSync(directory).map(function(name){
+    //everything is stored as complete directory name
+    return directory + '/' + name; 
+  })
+  
+  //filter only directories
+  return containedPaths.filter(function (path) {
+    return isDirectory(path);
+  });
+  
+
+}
+
+function getFiles(){      //simple iteration, calls getFilesInDirectory for each directory
+  settings.targetDirectories.forEach(function(dir){
+    settings.targetFiles = settings.targetFiles.concat(getFilesInDirectory(dir));
+  });
+}
+
+function getFilesInDirectory(directory){    //get files in one directory
+  return fs.readdirSync(directory).map(function(name){
+    return directory + '/' + name;
+  }).filter(function (name) {
+    return isJsFileString(name);
+  });
+}
 
 
+///////////////////STRING TEMPLATING
+
+
+function makeScriptsString(){       
+    settings.scriptsString = "";
+    settings.targetFiles.forEach(function (path) {
+      var script = `<script src="${path}"></script>`;
+      settings.scriptsString += ('\n' + script);
+    });
+}
+
+//fill the template string with the scripts tag and script names
+function makeMochaHtmlString() {     //!presently includes all mocha script tags, etc, even 
+  var scripts = settings.scriptsString;
+  var specfile = settings.creationDirectory + '/' + settings.specFileName;
+
+  var htmlString = `<!DOCTYPE>
+    <html>
+     
+      <head>
+        <meta charset="UTF-8">
+        <link href='https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.css' rel='stylesheet' />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src='https://cdn.rawgit.com/chaijs/chai/2.3.0/chai.js'></script>
+        <script src='https://cdn.rawgit.com/mochajs/mocha/2.2.5/mocha.js'></script>
+        <script>mocha.setup('bdd')</script>
+        
         ${scripts}
 
-      </body>
+        <script>
+          var assert = chai.assert,  
+          expect = chai.expect,
+          should = chai.should();
+          $(function() {
+            mocha.run();
+          })
+        </script>
 
-      </html>`;
+      </head>
 
-    return htmlString;
-  }
+    <body >
+      <div id="mocha"></div>
+    </body>
+
+    </html>`;
+
+  settings.htmlString = htmlString;
+}
+
+function makeHtmlString() {     
+  var scripts = settings.scriptsString;
+  var specfile = settings.creationDirectory + '/' + settings.specFileName;
+
+  var htmlString = `<!DOCTYPE>
+    <html>
+     
+      <head>
+        <meta charset="UTF-8">
+        
+        ${scripts}
+
+      </head>
+
+    <body >
+      
+    </body>
+
+    </html>`;
+
+  settings.htmlString = htmlString;
+}
+
+
+function makeSpecString(){
+
+  var str = "";
+
+  var describeEnd = "}); \n\n"
+  var expectStart = "  it('should ', function() { \n";
+  var expectEnd = "  })"; 
+  var spacer = "\n\n\n\n";
+
+  settings.targetFiles.forEach(function(file){
+    str += "describe(\'" + nameFromPath(file) + "\'" +", function() { \n" + spacer + expectStart + spacer +expectEnd + spacer + describeEnd; 
+  });
+
+  settings.specString = str; 
+
+}
+
+
+///////////////////FILE CREATION
+
+function writeSpecFile(){
+  fs.writeFile(settings.creationDirectory + settings.specFileName, settings.specString, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The file" + settings.specFileName + " was saved!");
+      spawn('open', [settings.directory + settings.specFileName]); //!not opening
+    }
+  });
+
+}
+
+function writeIndex(){
+  fs.writeFile(settings.creationDirectory + settings.htmlFileName, settings.htmlString, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The file" + settings.htmlFileName + " was saved!");
+      spawn('open', [settings.creationDirectory + settings.htmlFileName]);
+    }
+  });
+}
+
+///////////////////UTILITIES
+
+function nameFromPath(path){
+  return path.split('/').reverse()[0];
+}
+
+//test whether a file is a .js file
+function isJsFileString(name) {
+  return name.slice(name.length - 3) === '.js';
+}
+
+  // test whether Command line argument is a directory
+function isDirectory(name) {
+  return fs.statSync(name).isDirectory()
+}
+
+function flagForIgnore(){ //!
 
 }
 
